1a9f56dff371aacbb459d8c87400030f554c7e0d
Stefanos Carlström <jagot@users.noreply.github.com>
2022-09-30 13:54:42 +0200

    Ensure caching does not interfere with unit choice (#5)

--- test/runtests.jl
+++ test/runtests.jl
@@ -14,6 +14,23 @@ using UnitfulAtomic
     end
 end
 
+@testset "Caching mechanism" begin
+    # We need to test that the caching mechanism respects the unit
+    # choice, to not get wrong values, when subsequently querying with
+    # different units. We get around this, by storing each unit choice
+    # separately, which is slightly wasteful, but sure to work.
+    for (unit,ref) in [(u"cm^-1",159_855.9743297u"cm^-1"),
+                       (u"eV",19.81961484203u"eV"),
+                       (u"Ry",1.456714822454u"Ry"),
+                       (u"hartree",0.728357411225u"hartree")]
+        df = get_nist_data("He I", unit)
+
+        @test df.Configuration[2] == "1s.2s"
+        E1s_2s = df.Level[2]
+        @test E1s_2s ≈ ref
+    end
+end
+
 @testset "LaTeX table generation" begin
     df = get_nist_data("He I", u"hartree")
     mktempdir() do dir
