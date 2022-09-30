fca74c04af6dfb2316ad9e123a9678c35945898f
Stefanos Carlström <jagot@users.noreply.github.com>
2022-09-30 11:37:24 +0200

    Update package (#4)
    * Moved to Github Actions
    
    * Use units from UnitfulAtomic.jl
    
    * Cache downloaded data using Scratch.jl
    
    * Added compat entries
    
    * Fix parsing
    
    * Drop Julia < 1.8
    
    * Test generation of LaTeX tables

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,19 +1,24 @@
 using AtomicData
 using Test
 using Unitful
-
-@testset "Units" begin
-    @test isa(u"Ha", Unitful.FreeUnits)
-    @test isa(u"Ry", Unitful.FreeUnits)
-end
+using UnitfulAtomic
 
 @testset "Simple queries" begin
-    for unit in [u"cm^-1", u"eV", u"Ry", u"Ha"]
+    for unit in [u"cm^-1", u"eV", u"Ry", u"hartree"]
         for el in ["He I", "He II", "W I", "Xe I", "Xe II"]
             df = get_nist_data(el, unit)
             # Slightly bogus checks
             @test size(df,1) > 0
             @test size(df,2) > 0
         end
+    end
+end
+
+@testset "LaTeX table generation" begin
+    df = get_nist_data("He I", u"hartree")
+    mktempdir() do dir
+        filename = joinpath(dir, "HeI.tex")
+        latex_table(filename, df, "He I")
+        @test isfile(filename)
     end
 end
